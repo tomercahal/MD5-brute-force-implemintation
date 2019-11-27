4950e7dca8f0ad5c9eb1c40b3686bf42029f28fd
tomer <Tomercahal@gmail.com>
2019-11-27 20:51:02 +0200

    Started using semaphore, having some problems with the client side...

--- MD5_Client.py
+++ MD5_Client.py
@@ -3,10 +3,10 @@ import time
 import multiprocessing
 import hashlib
 import time
+import os
 
 
 #############################################  ANSWER IS: 3735928559 ###################################################
-
 
 def check_hash(num):
     """This function gets a number as the input and returns the hash code of the number that was the input."""
@@ -17,87 +17,78 @@ def check_hash(num):
     return hash
 
 
-def run_over_range(start_num, end_num, hash_needed, que, found):
-    """This function is called on each process and it goes over the range of the numbers that
-     the process needs to go over"""
-    while start_num != end_num:
-        if found:
-            break
-        hash_of_num = check_hash(start_num)
-        if hash_of_num == hash_needed:
-            print 'number found!'
-            que.put(start_num)  # inserting the value of the number into a queue
-            return
-        start_num += 1
-
-
 class Client (object):
     def __init__(self):
         """The constructor of the Client class"""
         self.IP = '127.0.0.1'  # The Ip of the client.
         self.PORT = 220  # The port of the client.
         self.cores = 0  # A variable that contains the amount of cores the pc has
+        self.processes_running = []  # A list of all the processes running at the time
         self.found = False
 
-    def start(self, run):
+    def start(self):
         """Sort of like the main function, it binds a socket connection to the server and gets the jobs that he asks."""
-        if run == 0:
-            try:
-                print('connecting to IP %s PORT %s' % (self.IP, self.PORT))
-                # Create a TCP/IP socket
-                global sock
-                sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                sock.connect((self.IP, self.PORT))
+        try:
+            print('connecting to IP %s PORT %s' % (self.IP, self.PORT))
+            # Create a TCP/IP socket
+            global sock
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sock.connect((self.IP, self.PORT))
 
-                print('connected to server')
-                msg = sock.recv(1024)
-                print('received message: %s' % msg.decode())
-                sock.sendall('Hello this is client, send me a job'.encode())
-
-            except socket.error as e:
-                print(e)
-                sock.close()
-        else:
+            print('connected to server')
+            msg = sock.recv(1024)
+            print('received message: %s' % msg.decode())
             self.cores = multiprocessing.cpu_count()
             print "The amount of cores this client has is: " + str(self.cores)
-            job = sock.recv(1024)
+            self.handle_server_job(sock)
 
-            result = self.handle_server_job(sock, job)
-            return result
+        except socket.error as e:
+            print(e)
+            sock.close()
 
-    def handle_server_job(self, server_socket, job):
+    def run_over_range(self, start_num, end_num, hash_needed, que):
+        """This function is called on each process and it goes over the range of the numbers that
+         the process needs to go over"""
+        while not self.found and start_num != end_num:  # If number not found yet or start num isn't equal to end num
+            hash_of_num = check_hash(start_num)
+            if hash_of_num == hash_needed:
+                print 'number found!'
+                que.put(start_num)  # inserting the value of the number into a queue
+                self.found = True
+                break
+            start_num += 1
+
+    def handle_server_job(self, server_socket):
         print 'im in handle server job'
-        parts_of_job = job.split('/')  # This splits according to the the protocol the server transfers the details.
-        print parts_of_job
-        OG_HASH = parts_of_job[0]  # The hash that we will need to compare to.
-        start_num = int(parts_of_job[1])
-        finish_num = int(parts_of_job[2])
-        each_core_work = (finish_num-start_num)/self.cores  # how many numbers will each core(process) go over in order to be efficient.
-        print start_num, finish_num, each_core_work
-        i = 0
-        que = multiprocessing.Queue()
-        p = ''
-        for i in range(self.cores):
-            print 'Started thread number: ' + str(i)
-            p = multiprocessing.Process(target=run_over_range, args=(start_num + i * each_core_work, start_num +
-                                                                     (i+1) * each_core_work, OG_HASH, que, self.found))
-            p.start()  # leatting the thread start running in the background.
-        p.join()  # waiting until the last one finishes their thread.
-        if not que.empty():  # if there is a number in the queue it means that we found the number
-            number_found = que.get()
-            server_socket.send('True/' + str(number_found))
-            return 'found'
-        else:
-            server_socket.send('False/None')
-            return ''
+        while not self.found:
+            job = sock.recv(1024)  # Getting the range and hash from the server
+            print 'The job is: ' + str(job)
+            parts_of_job = job.split('/')  # This splits according to the the protocol the server transfers the details.
+            print parts_of_job
+            OG_HASH = parts_of_job[0]  # The hash that we will need to compare to.
+            start_num = int(parts_of_job[1])
+            finish_num = int(parts_of_job[2])
+            each_core_work = 1 + ((finish_num-start_num)/self.cores) # how many numbers will each core(process) go over in order to be efficient.
+            print start_num, finish_num, each_core_work
+            que = multiprocessing.Queue()
+            p = ''
+            for i in range(self.cores):
+                print 'Started process number: ' + str(i+1)  # Adding 1 so it would not start from 0
+                print 'These are the args:\r\n' + str(start_num + (i * each_core_work)), \
+                    str(start_num + ((i+1) * each_core_work)), OG_HASH, que, self.found
+                p = multiprocessing.Process(target=self.run_over_range, args=(start_num + i * each_core_work, start_num +
+                                                                         (i+1) * each_core_work, OG_HASH, que))
+                p.start()  # Letting the thread start running in the background.
+
+            p.join()  # Waiting for the last thread to finish up
+
+            if not que.empty():  # if there is a number in the queue it means that we found the number
+                number_found = que.get()
+                server_socket.send('True/' + str(number_found))  # Send to server, number found
+            else:
+                server_socket.send('False/None')  # Send to server number has not been found
 
 
 if __name__ == '__main__':
-    run = 0  # since on the other runs we don't need to accept a new socket anymore, we stay connected.
-    c = Client()
-    while True:
-        still = c.start(run)
-        run += 1
-        if still == 'found':
-            while True:
-                pass
+    c = Client()  # Creating a new client
+    c.start()  # Starting the client
--- MD5_Server.py
+++ MD5_Server.py
@@ -4,8 +4,9 @@ import time
 import threading
 import multiprocessing
 import os
-
+import sys
 #############################################  ANSWER IS: 3735928559 ###################################################
+ALL_SOCKETS_IN_USE = 'All sockets are being used at the time please wait'  # Used when all there are 10 users logged
 
 
 class Server (object):
@@ -13,65 +14,82 @@ class Server (object):
         """The constructor of the Server"""
         self.IP = '127.0.0.1'  # The IP of the server.
         self.PORT = 220  # The chose port to have the connection on
-        self.HASH = 'EC9C0F7EDCC18A98B1F31853B181330'  # This is the hash that we need to match
-        self.OPEN_SOCKETS = []  # A list of all the sockets that are currently in use and open.
-        self.START_NUM = 1000000000  # The number that the hash starts with
+        self.HASH = 'EC9C0F7EDCC18A98B1F31853B181330'  # This is the hash that we need to match (the answer md5d)
+        self.START_NUM = 3735928259  # The number that the hash starts with
         self.FINISH_NUM = 9999999999  # The number that the hash ends with
         self.CHOSEN_RANGE = 10000  # This is the range that the client will go over each time
         self.HASH_FOUND = 'The hash has been found! it is: '  # Just a simple message it will be used later
+        self.found = False  # This will change if the hash has been 5 (some client found the md5)
+        self.active_users = []  # This is a list containing the threads that are active and if there are any open
+        self.users_allowed = sys.maxint   # This is the amount of users that are allowed to be logged in at the same time
+        self.sem = threading.Semaphore(self.users_allowed)  # users allowed is a variable making the program dynamic
+
+    def connect_client(self, client_sock):
+        """This function knows how to handle many clients coming in, it let's a user know if all the sockets are full
+        and he has to wait. It returns the thread number that it will be assigned later."""
+        connection_available = True
+        printed_once = False
+        while connection_available:  # used so
+            for user in range(self.users_allowed):
+                if self.active_users[user] == 0:
+                    self.active_users[user] = self.active_users.index(0) + 1
+                    return self.active_users[user]  # We have found a number that can be assigned to the thread
+            if not printed_once:  # If we have not send a reply to the user yet
+                print ALL_SOCKETS_IN_USE
+                client_sock.send(ALL_SOCKETS_IN_USE)
+                printed_once = True  # Now the user won't get the message anymore
+
+    def handle_thread(self, client_sock, thread_num):
+        """ This function handles the clients. Since only users_allowed (10 at the time), can be connected and send
+        requests at a time."""
+        print 'You are thread number: ' + str(thread_num)
+        self.sem.acquire()  # Decreases the users logged in at the time (new thread opened)
+        print 'New client connected to the database + ' + str(self.sem._Semaphore__value) + ' sockets left\r\n'
+        self.send_to_client(client_sock)
 
     def start(self):
         """Another main function in the server side, It is mainly used to aceept new clients through creating sockets
         and then directing the code to assaign them their jobs to find."""
         try:
-            # check_sockets_active(OPEN_SOCKETS)  # change if got time to make sure every number is ran
+            # check_sockets_active(active_users)  # change if got time to make sure every number is ran
 
             print('Server starting up on ip %s port %s' % (self.IP, self.PORT))
             # Create a TCP/IP socket
             sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             sock.bind((self.IP, self.PORT))
             sock.listen(1)
-            while True:
-                print('Waiting for a new client')
+            thread_number = 0  # This will be sort of an ID for the thread, used in debugging
+            while True:  # Keep accepting as many people that want to help (speed up the process)
+                thread_number += 1  # Increasing cause of new client
+                print('\r\nWaiting for a new client')
                 client_socket, client_address = sock.accept()  # Last step of being on a socket
                 print('New client entered!')
                 client_socket.sendall('Hello this is server'.encode())
-                self.OPEN_SOCKETS.append(client_socket)  # Adding the new socket to the list
-                msg = client_socket.recv(1024)
-                print('Received message: %s' % msg.decode())
-                for s in self.OPEN_SOCKETS:
-                    thread_each_socket = threading.Thread(target=self.handle_client, args=(s,))  # Creating a thread
-                    thread_each_socket.start()
+                thread = threading.Thread(target=self.handle_thread, args=(client_socket, thread_number))
+                thread.start()  # Starting the thread
 
         except socket.error as e:
             print(e)
-
-    def handle_client(self, client_socket):
-        """This function is the main function that calls all the other ones. It is sort of like the manager. From here
-        we will send a new range to the clients and get their calculations to see it they have the match. If they don't
-        we will send them a new set of numbers, till we don't have anymore left."""
-        while self.START_NUM < self.FINISH_NUM:
-            thread = threading.Thread(target=self.send_to_client, args=(client_socket,))  # Creating the thread
-            thread.start()  # activating the thread and now it goes to send_to_client func
-            thread_recv = threading.Thread(target=self.receive_from_client, args=(client_socket,))  # The receiving thread, gets input from client
-            thread_recv.start()
-            thread_recv.join()  # waiting for the receiving thread, cause we must get an answer if it was in the range.
-            self.START_NUM += self.CHOSEN_RANGE  # increasing the global variable until we get to finish num.
 
     def send_to_client(self, client_socket):
         """In this function I will send the client the job he has do to(basically his part) in
          finding the 10 digit number.
          The format of the data will be: [hash code wanted]/start of job/end of job"""
-        data_to_send = "%s/%s/%s" % (self.HASH.lower(), self.START_NUM, self.START_NUM + self.CHOSEN_RANGE)
-        print 'Sending to the client: ' + data_to_send  # The data that is sent to the client
-        client_socket.sendall(data_to_send)
+        while not self.found and self.START_NUM < self.FINISH_NUM:  # While no client found the md5 hash value or max num
+            data_to_send = "%s/%s/%s" % (self.HASH.lower(), self.START_NUM, self.START_NUM + self.CHOSEN_RANGE)
+            print 'Sending to the client: ' + data_to_send  # The data that is sent to the client
+            client_socket.send(data_to_send)  # Send to the client the range he needs to check
+            self.receive_from_client(client_socket)
+            self.START_NUM += self.CHOSEN_RANGE  # Increasing the global variable until we get to finish num.
+        client_socket.send('Thank you for helping us find the number we were looking for! Cheers!')
+        self.sem.release()  # Releasing the client, no longer need him.
 
     def receive_from_client(self, client_socket):
         """In this function I will receive the status of the completed job from the client. He will send me
         back the status of the job using this format: [Bool]/None or the number if found"""
-        response = client_socket.recv(1024)
-        parts = response.split('/')
-        print parts
+        response = client_socket.recv(1024)  # Getting the response from the user.
+        parts = response.split('/')  # Splitting the user's answer.
+        print 'this is parts: it is a list ' + str(parts)
         bool_found = parts[0]
         print parts[0], 'num is: ' + parts[1]
         if not bool_found:  # if the number is found WEIRDD
